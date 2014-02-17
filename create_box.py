d3f2c3b4270485a84b11008d06f84e75dd9a5d0d
ant6 <ant6ant@gmail.com>
2014-02-17 01:49:57 +0100

    map_input for create_box from file

--- create_box.py
+++ create_box.py
@@ -2,25 +2,10 @@ __author__ = 'maciex'
 
 import numpy
 
-#a = (100,100)
-#b = (200,300)
-#c = (300,200)
-#d = (400,400)
-
-
-a = (210,268)
-b = (385,216)
-c = (406,473)
-d = (596,394)
-
 DIM_X =  1024
 DIM_Y =  728
 
-POINTS = ( a , b , c , d )
-
-def create_box ( DIM_X , DIM_Y , POINTS ):
-    matrix = numpy.zeros(shape=(DIM_X,DIM_Y))
-
+def create_box ( DIM_X , DIM_Y , POINTS , matrix ):
 
     def solve( (x1 , y1) , (x2 , y2) ):
         ARRAY_1 = numpy.array([[x1,1], [x2,1]])
@@ -69,24 +54,52 @@ def create_box ( DIM_X , DIM_Y , POINTS ):
                 if ( ( j < ( line4[0]*i + line4[1] )) and ( j > ( line3[0]*i + line3[1] )) ):
                     matrix[i,j] = 1
 
-
-
-
     return matrix
 
 
+def load_box( DIM_X , DIM_Y , map_input_path ):
+    with open(map_input_path, 'r') as map_input:
+        map_input = map_input.readlines()
+        map_input = [ e.strip() for e in map_input ]
+        map_output = []
+        #wywalanie komentarzy
+        for e in map_input:
+            if '#' not in e:
+                comma = e.index(",")
+                map_output.append(int(e[:comma]))
+                map_output.append(int(e[comma+1:]))
+        map_len = len(map_output)
+        #sprawdzamy czy mamy krotnosc 8 linii poza komentarzami
+        if map_len % 8 != 0:
+            print "map input format error"
+            raise ValueError
+        
+        matrix = numpy.zeros(shape=(DIM_X,DIM_Y))
+        for i in range(0,len(map_output),8):
+            print i
+            a = (map_output[i], map_output[i+1])
+            b = (map_output[i+2], map_output[i+3])
+            c = (map_output[i+4], map_output[i+5])
+            d = (map_output[i+6], map_output[i+7])
+            points = (a, b, c, d)
+            matrix = create_box(DIM_X, DIM_Y, points, matrix)
+            
+        return matrix
+
+
+#python createbox
 if __name__ == "__main__":
 
-    matrix = create_box(DIM_X, DIM_Y, POINTS)
-
     import pygame
+
+    matrix = load_box( DIM_X , DIM_Y , "map_input.txt" )
 
     pygame.init()
 
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
 
-    setDisplay = pygame.display.set_mode((DIM_X,DIM_Y))
+    setDisplay = pygame.display.set_mode((DIM_X, DIM_Y))
     setDisplay.fill(WHITE)
 
     pixele = pygame.PixelArray(setDisplay)
@@ -97,7 +110,4 @@ if __name__ == "__main__":
             else:
                 pixele[n][m] = WHITE
 
-
     pygame.display.update()
-
-    raw_input()
