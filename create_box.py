bb27a5ed1e4caa890b56abdac114c6f2ff2b1260
ant6 <ant6ant@gmail.com>
2014-02-17 02:11:30 +0100

    zarąbiste kolorki (numerowane pomieszczenia)

--- create_box.py
+++ create_box.py
@@ -5,7 +5,7 @@ import numpy
 DIM_X =  1024
 DIM_Y =  728
 
-def create_box ( DIM_X , DIM_Y , POINTS , matrix ):
+def create_box ( DIM_X , DIM_Y , POINTS , chamber, matrix ):
 
     def solve( (x1 , y1) , (x2 , y2) ):
         ARRAY_1 = numpy.array([[x1,1], [x2,1]])
@@ -21,17 +21,17 @@ def create_box ( DIM_X , DIM_Y , POINTS , matrix ):
         for i in range(POINTS[0][0],POINTS[1][0]):
             for j in range(0,DIM_Y):
                 if ( ( j < ( line1[0]*i + line1[1] )) and ( j > ( line2[0]*i + line2[1] )) ):
-                    matrix[i,j] = 1
+                    matrix[i,j] = chamber
 
         for i in range(POINTS[1][0],POINTS[2][0]):
             for j in range(0,DIM_Y):
                 if ( ( j < ( line4[0]*i + line4[1] )) and ( j > ( line2[0]*i + line2[1] )) ):
-                    matrix[i,j] = 1
+                    matrix[i,j] = chamber
 
         for i in range(POINTS[2][0],POINTS[3][0]):
             for j in range(0,DIM_Y):
                 if ( ( j < ( line4[0]*i + line4[1] )) and ( j > ( line3[0]*i + line3[1] )) ):
-                    matrix[i,j] = 1
+                    matrix[i,j] = chamber
 
     else:
         line2 = solve( POINTS[0] , POINTS[1] )
@@ -42,17 +42,17 @@ def create_box ( DIM_X , DIM_Y , POINTS , matrix ):
         for i in range(POINTS[0][0],POINTS[1][0]):
             for j in range(0,DIM_Y):
                 if ( ( j < ( line1[0]*i + line1[1] )) and ( j > ( line2[0]*i + line2[1] )) ):
-                    matrix[i,j] = 1
+                    matrix[i,j] = chamber
 
         for i in range(POINTS[1][0],POINTS[2][0]):
             for j in range(0,DIM_Y):
                 if ( ( j < ( line1[0]*i + line1[1] )) and ( j > ( line3[0]*i + line3[1] )) ):
-                    matrix[i,j] = 1
+                    matrix[i,j] = chamber
 
         for i in range(POINTS[2][0],POINTS[3][0]):
             for j in range(0,DIM_Y):
                 if ( ( j < ( line4[0]*i + line4[1] )) and ( j > ( line3[0]*i + line3[1] )) ):
-                    matrix[i,j] = 1
+                    matrix[i,j] = chamber
 
     return matrix
 
@@ -70,19 +70,20 @@ def load_box( DIM_X , DIM_Y , map_input_path ):
                 map_output.append(int(e[comma+1:]))
         map_len = len(map_output)
         #sprawdzamy czy mamy krotnosc 8 linii poza komentarzami
-        if map_len % 8 != 0:
+        if map_len % 10 != 0:
             print "map input format error"
             raise ValueError
-        
+
         matrix = numpy.zeros(shape=(DIM_X,DIM_Y))
-        for i in range(0,len(map_output),8):
+        for i in range(0,len(map_output),10):
             print i
-            a = (map_output[i], map_output[i+1])
-            b = (map_output[i+2], map_output[i+3])
-            c = (map_output[i+4], map_output[i+5])
-            d = (map_output[i+6], map_output[i+7])
+            chamber = map_output[i]
+            a = (map_output[i+2], map_output[i+3])
+            b = (map_output[i+4], map_output[i+5])
+            c = (map_output[i+6], map_output[i+7])
+            d = (map_output[i+8], map_output[i+9])
             points = (a, b, c, d)
-            matrix = create_box(DIM_X, DIM_Y, points, matrix)
+            matrix = create_box(DIM_X, DIM_Y, points, chamber, matrix)
             
         return matrix
 
@@ -98,6 +99,10 @@ if __name__ == "__main__":
 
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    YELLOW = (255, 255, 0)
 
     setDisplay = pygame.display.set_mode((DIM_X, DIM_Y))
     setDisplay.fill(WHITE)
@@ -107,7 +112,11 @@ if __name__ == "__main__":
         for m in range(0, DIM_Y):
             if matrix[n][m] == 0:
                 pixele[n][m] = BLACK
-            else:
+            elif matrix[n][m] == 1:
                 pixele[n][m] = WHITE
+            elif matrix[n][m] == 2:
+                pixele[n][m] = YELLOW
+            elif matrix[n][m] == 3:
+                pixele[n][m] = RED
 
     pygame.display.update()
